8469f5abdd9e728ddc12c1e8a5d5e76b79fef217
Federico Vaga <federico.vaga@gmail.com>
2016-02-16 19:17:23 +0100

    add comment to skeleton program
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- examples/skeleton.c
+++ examples/skeleton.c
@@ -10,6 +10,18 @@ static void skel_tear_down(struct m_suite *m_suite)
 	/* Clean the environment */
 }
 
+
+static void skel_test_setup(struct m_test *m_test)
+{
+	/* Test set up */
+}
+
+static void skel_test_tear_down(struct m_test *m_test)
+{
+	/* Test tear down */
+}
+
+
 static void skel_test1(struct m_test *m_test)
 {
 	/* First Test */
@@ -23,10 +35,19 @@ static void skel_test2(struct m_test *m_test)
 
 int main(int argc, char *argv[])
 {
+	/*
+	 * Declare all your test collections. Use the m_test()
+	 * macro to symplify the declaration:
+	 *     m_test(set_up, test, tear_down)
+	 */
 	struct m_test skel_tests[] = {
-		m_test(NULL, skel_test1, NULL),
+		m_test(skel_test_setup, skel_test1, skel_test_tear_down),
 		m_test(NULL, skel_test2, NULL),
 	};
+
+	/*
+	 * Declare the test suite and assign your test collection to it
+	 */
 	struct m_suite skel_suite = {
 		.name = "skeleton test",
 		.flags = 0,
@@ -36,6 +57,9 @@ int main(int argc, char *argv[])
 		.tear_down = skel_tear_down,
 	};
 
+	/*
+	 * Execute the test suite
+	 */
 	m_suite_run(&skel_suite);
 
 	return 0;
