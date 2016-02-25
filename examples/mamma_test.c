338c15ed2777f8f1e3050e6ae91adae0f216acbe
Federico Vaga <federico.vaga@gmail.com>
2016-02-25 23:42:50 +0100

    example: use all checks functions
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- examples/mamma_test.c
+++ examples/mamma_test.c
@@ -39,26 +39,7 @@ static void test1(struct m_test *m_test)
 
 static void test2(struct m_test *m_test)
 {
-	m_check_int_eq(4, 5);
-	m_check_int_neq(4, 4);
-	m_check_int_gt(5, 6);
-	m_check_int_ge(5, 6);
-	m_check_int_ge(5, 6);
-	m_check_int_lt(4, 3);
-	m_check_int_le(4, 3);
-	m_check_int_le(5, 3);
-	m_check_int_range(0, 10, 10);
-	m_check_int_nrange(0, 10, 5);
-	m_check_dbl_eq(4.1234562, 4.123456);
-	m_check_dbl_neq(4.214, 4.214);
-	m_check_dbl_gt(5.12433522, 6.023545);
-	m_check_dbl_ge(5.12312, 6.1231);
-	m_check_dbl_ge(5.556, 5.5561);
-	m_check_dbl_lt(4.34234, 3.93243);
-	m_check_dbl_le(4.143234, 3.54352);
-	m_check_dbl_le(5.1232, 5.1231);
-	m_check_dbl_range(0, 1, 1.2425435);
-	m_check_dbl_nrange(0, 1, 0.12312);
+
 }
 
 static void test3(struct m_test *m_test)
@@ -101,9 +82,142 @@ static void test6(struct m_test *m_test)
 	m_assert_int_neq(4, 4);
 }
 
-static void test_single_ok(struct m_test *m_test)
+/**
+ * It uses all checks
+ */
+static void test_check_only(struct m_test *m_test)
 {
-	m_assert_int_neq(4, 4);
+	/* Integers */
+
+	m_check_int_eq(4, 5); /* Err */
+	m_check_int_eq(4, 4);
+
+	m_check_int_neq(4, 5);
+	m_check_int_neq(4, 4); /* Err */
+
+	m_check_int_gt(5, 4);
+	m_check_int_gt(5, 5); /* Err */
+	m_check_int_gt(5, 6); /* Err */
+
+	m_check_int_ge(5, 4);
+	m_check_int_ge(5, 5);
+	m_check_int_ge(5, 6); /* Err */
+
+	m_check_int_lt(5, 6);
+	m_check_int_lt(5, 5); /* Err */
+	m_check_int_lt(5, 4); /* Err */
+
+	m_check_int_le(5, 6);
+	m_check_int_le(5, 5);
+	m_check_int_le(5, 4); /* Err */
+
+	m_check_int_range(0, 10, 11); /* Err */
+	m_check_int_range(0, 10, 5);
+
+	m_check_int_nrange(0, 10, 11);
+	m_check_int_nrange(0, 10, 5); /* Err */
+
+	/* Double */
+
+	m_check_dbl_eq(4.123456, 4.123456);
+	m_check_dbl_eq(4.123456, 4.123457); /* Err */
+
+	m_check_dbl_neq(4.123456, 4.123456); /* Err */
+	m_check_dbl_neq(4.123456, 4.123457);
+
+	m_check_dbl_gt(4.123456, 4.123455);
+	m_check_dbl_gt(4.123456, 4.123456); /* Err */
+	m_check_dbl_gt(4.123456, 4.123457); /* Err */
+
+	m_check_dbl_ge(4.123456, 4.123455);
+	m_check_dbl_ge(4.123456, 4.123456);
+	m_check_dbl_ge(4.123456, 4.123457); /* Err */
+
+	m_check_dbl_lt(4.123456, 4.123457);
+	m_check_dbl_lt(4.123456, 4.123456); /* Err */
+	m_check_dbl_lt(4.123456, 4.123455); /* Err */
+
+	m_check_dbl_le(4.123456, 4.123457);
+	m_check_dbl_le(4.123456, 4.123456);
+	m_check_dbl_le(4.123456, 4.123455); /* Err */
+
+	m_check_dbl_range(0, 1, 1.2425435); /* Err */
+	m_check_dbl_range(0, 1, 0.2425435);
+
+	m_check_dbl_nrange(0, 1, 1.2425435);
+	m_check_dbl_nrange(0, 1, 0.2425435); /* Err */
+
+	/* Memory - use string to make it simple */
+
+	m_check_mem_not_null(NULL); /* Err */
+	m_check_mem_not_null("bbb");
+
+	m_check_mem_null(NULL);
+	m_check_mem_null("bbb"); /* Err */
+
+	m_check_mem_eq("bbb", "bbb", 4);
+	m_check_mem_eq("bbb", "aaa", 4); /* Err */
+
+	m_check_mem_neq("bbb", "aaa", 4);
+	m_check_mem_neq("bbb", "bbb", 4); /* Err */
+
+	m_check_mem_gt("bbb", "aaa", 4);
+	m_check_mem_gt("bbb", "bbb", 4); /* Err */
+	m_check_mem_gt("bbb", "ccc", 4); /* Err */
+
+	m_check_mem_ge("bbb", "aaa", 4);
+	m_check_mem_ge("bbb", "bbb", 4);
+	m_check_mem_ge("bbb", "ccc", 4); /* Err */
+
+	m_check_mem_lt("bbb", "ddd", 4);
+	m_check_mem_lt("bbb", "bbb", 4); /* Err */
+	m_check_mem_lt("bbb", "aaa", 4); /* Err */
+
+	m_check_mem_le("bbb", "ddd", 4);
+	m_check_mem_le("bbb", "bbb", 4);
+	m_check_mem_le("bbb", "aaa", 4); /* Err */
+
+	m_check_mem_range("bbb", "ddd", "aaa", 4); /* Err */
+	m_check_mem_range("bbb", "ddd", "ccc", 4);
+
+	m_check_mem_nrange("bbb", "ddd", "aaa", 4);
+	m_check_mem_nrange("bbb", "ddd", "ccc", 4);  /* Err */
+
+	/* Strings */
+
+	m_check_str_not_null(NULL); /* Err */
+	m_check_str_not_null("bbb");
+
+	m_check_str_null(NULL);
+	m_check_str_null("bbb"); /* Err */
+
+	m_check_str_eq("bbb", "bbb", 4);
+	m_check_str_eq("bbb", "aaa", 4); /* Err */
+
+	m_check_str_neq("bbb", "aaa", 4);
+	m_check_str_neq("bbb", "bbb", 4); /* Err */
+
+	m_check_str_gt("bbb", "aaa", 4);
+	m_check_str_gt("bbb", "bbb", 4); /* Err */
+	m_check_str_gt("bbb", "ccc", 4); /* Err */
+
+	m_check_str_ge("bbb", "aaa", 4);
+	m_check_str_ge("bbb", "bbb", 4);
+	m_check_str_ge("bbb", "ccc", 4); /* Err */
+
+	m_check_str_lt("bbb", "ddd", 4);
+	m_check_str_lt("bbb", "bbb", 4); /* Err */
+	m_check_str_lt("bbb", "aaa", 4); /* Err */
+
+	m_check_str_le("bbb", "ddd", 4);
+	m_check_str_le("bbb", "bbb", 4);
+	m_check_str_le("bbb", "aaa", 4); /* Err */
+
+	m_check_str_range("bbb", "ddd", "aaa", 4); /* Err */
+	m_check_str_range("bbb", "ddd", "ccc", 4);
+
+	m_check_str_nrange("bbb", "ddd", "aaa", 4);
+	m_check_str_nrange("bbb", "ddd", "ccc", 4);  /* Err */
 }
 
 static void test_single_ko(struct m_test *m_test)
@@ -115,7 +229,7 @@ static void test_single_ko(struct m_test *m_test)
 
 int main(int argc, char *argv[])
 {
-	struct m_test test_sng_ok = m_test(NULL, test_single_ok, NULL);
+	struct m_test test_check = m_test(NULL, test_check_only, NULL);
 	struct m_test test_sng_ko = m_test(NULL, test_single_ko, NULL);
 	struct m_test tests[] = {
 		m_test(set_up, test1, tear_down),
@@ -136,9 +250,9 @@ int main(int argc, char *argv[])
 	};
 
 	m_suite_run(&suite);
-
-	m_test_run(&test_sng_ok);
 	m_test_run(&test_sng_ko);
+
+	m_test_run(&test_check);
 
 	return 0;
 }
