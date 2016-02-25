a2975d0541b39415c8f965bd5a69554add2c4a98
Federico Vaga <federico.vaga@gmail.com>
2016-02-26 00:31:17 +0100

    mamma_test: mamma self-test using assert()
    Signed-off-by: Federico Vaga <federico.vaga@gmail.com>

--- examples/mamma_test.c
+++ examples/mamma_test.c
@@ -4,83 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <assert.h>
 #include <mamma.h>
 
-static void set_up(struct m_test *m_test)
-{
-}
-static void tear_down(struct m_test *m_test)
-{
-}
-
-static void test1(struct m_test *m_test)
-{
-	m_assert_int_eq(4, 4);
-	m_assert_int_neq(4, 5);
-	m_assert_int_gt(5, 4);
-	m_assert_int_ge(5, 4);
-	m_assert_int_ge(5, 5);
-	m_assert_int_lt(4, 5);
-	m_assert_int_le(4, 5);
-	m_assert_int_le(5, 5);
-	m_assert_int_range(0, 10, 4);
-	m_assert_int_nrange(0, 10, 15);
-	m_assert_dbl_eq(4.123456, 4.123456);
-	m_assert_dbl_neq(4.21453523, 4.657454);
-	m_assert_dbl_gt(5.12433522, 5.023545);
-	m_assert_dbl_ge(5.12312, 4.1231);
-	m_assert_dbl_ge(5.556, 5.556);
-	m_assert_dbl_lt(4.34234, 4.93243);
-	m_assert_dbl_le(4.143234, 4.54352);
-	m_assert_dbl_le(5.123, 5.123);
-	m_assert_dbl_range(0, 1, 0.12425435);
-	m_assert_dbl_nrange(0, 1, 1.12312);
-}
-
-static void test2(struct m_test *m_test)
-{
-
-}
-
-static void test3(struct m_test *m_test)
-{
-	int a = 4, b = 5;
-
-	m_check_custom(a == b || a == 1, EINVAL,
-		       "%d != %d and %d != 1", a, b, a);
-	m_assert_custom(a == b || a == 1, EINVAL,
-			"%d != %d and %d != 1", a, b, a);
-	m_assert_custom(a == b || a == 1, EINVAL,
-			"%d != %d and %d != 1", a, b, a);
-}
-
-static void test4(struct m_test *m_test)
-{
-	void *ptr = NULL;
-
-	m_assert_mem_not_null(m_test);
-	m_assert_mem_not_null(ptr);
-}
-
-static void test5(struct m_test *m_test)
-{
-	void *ptr = NULL;
-
-	m_assert_mem_null(ptr);
-	m_assert_mem_null(m_test);
-}
-
-static void test6(struct m_test *m_test)
-{
-	m_assert_int_neq(4, 5);
-	m_assert_int_neq(4, 6);
-	m_skip(4 == 5);
-	m_assert_int_neq(4, 7);
-	m_skip(5 == 5);
-	m_assert_int_neq(4, 4);
-	m_assert_int_neq(4, 5);
-	m_assert_int_neq(4, 4);
-}
 
 /**
  * It uses all checks
@@ -314,28 +240,15 @@ static void test_check_only(struct m_test *m_test)
 	m_check_str_nrange("bbb", "ddd", "ccc", 4);  /* Err */
 }
 
-static void test_single_ko(struct m_test *m_test)
-{
-	m_assert_int_neq(4, 4);
-	m_assert_int_neq(4, 5);
-}
-
 
 int main(int argc, char *argv[])
 {
-	struct m_test test_check = m_test(NULL, test_check_only, NULL);
-	struct m_test test_sng_ko = m_test(NULL, test_single_ko, NULL);
-	struct m_test tests[] = {
+		struct m_test tests[] = {
 		m_test(NULL, test_good_assert, NULL),
-		m_test(set_up, test1, tear_down),
-		m_test(NULL, test2, NULL),
-		m_test(NULL, test3, NULL),
-		m_test(NULL, test4, NULL),
-		m_test(NULL, test5, NULL),
-		m_test(NULL, test6, NULL),
+		m_test(NULL, test_check_only, NULL),
 	};
 	struct m_suite suite = {
-		.name = "cut main test suite\0",
+		.name = "Mamma auto-test",
 		.flags = M_VERBOSE | M_ERRNO,
 		.private = NULL,
 		.tests = tests,
@@ -345,9 +258,11 @@ int main(int argc, char *argv[])
 	};
 
 	m_suite_run(&suite);
-	m_test_run(&test_sng_ko);
 
-	m_test_run(&test_check);
+	assert(0 == tests[0].warnings);
+	assert(M_STATE_EXIT_SUCCESS == tests[0].exit);
+	assert(44 == tests[1].warnings);
+	assert(M_STATE_EXIT_SUCCESS == tests[0].exit);
 
 	return 0;
 }
